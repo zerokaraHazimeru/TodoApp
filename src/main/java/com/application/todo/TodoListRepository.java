440ba7618ab177d452aba160ba906c517fb28504
ad <daikonap@yahoo.co.jp>
2020-09-23 21:31:17 +0900

    change about setting of db

---
+++ src/main/java/com/application/todo/TodoListRepository.java
@@ -0,0 +1,13 @@
+package com.application.todo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TodoListRepository extends JpaRepository<TodoListEntity,Integer>{
+	
+	TodoListEntity findByTodoNum(Integer todoNum);
+
+}
+
+
